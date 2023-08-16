bb85beb8d5f6e09863054631d64dcb5af7980ed6
TaurVil <41305538+TaurVil@users.noreply.github.com>
2023-08-16 12:06:51 -0500

    Update 14.plot_2A.R
    Updated for response.

--- code/14.plot_2A.R
+++ code/14.plot_2A.R
@@ -1,15 +1,9 @@
 library(ggplot2); library(data.table); library(patchwork)
 ################## Read in data, set offset ########################
-load("./DATA/pvalues_1mil_perms.RData")
+load("../DATA/pvalues.RData")
 
 offset=9e-7; method="ML"
 res <- res[apply(abs(res[,which(colnames(res) %like% "delta" & colnames(res) %like% method)]),1,min) > 0,]
-
-# set p-value as 1 if directional conditions aren't met
-res$double_plus_sign.pval.ML[sign(res[[paste0("delta_L13.",method)]]) != sign(res[[paste0("delta_D13.",method)]])] <- 1
-
-res$double_sign_during.pval.ML[sign(res[[paste0("delta_L13.",method)]]) != sign(res[[paste0("delta_D13.",method)]]) | 
-                                 sign(res[[paste0("delta_L13.",method)]]) == sign(res[[paste0("delta_L12.",method)]])] <- 1
 
 table(res$type)
 
@@ -17,11 +11,17 @@ original_sites <- c("chr5_96244549",
                     "chr2_204738938",
                     "chr5_114915460", 
                     "chr18_77287776")
+
+# calculate p-value from L & D independently
+xi <- -2*(log(res$L_1v3.pval.ML.conditioned) + log(res$D_1v3.pval.ML.conditioned))
+res$p.conditioned <- 1 - pchisq(xi, 4)
+
+
 ####################
 
-res2 <- res[sign(res$delta_L13.ML) == sign(res$delta_D13.ML) & sign(res$delta_L13.ML) != sign(res$delta_L12.ML) & res$maf.ML >= 0,]
+res2 <- res
 
-res2$logp <- -log10(res2$double_sign_during.pval.ML + offset)
+res2$logp <- -log10(res2$p.conditioned + offset)
 res2$rank <- rank(res2$logp)/length(res2$logp)
 
 res2$sh <- NA; 
@@ -52,9 +52,9 @@ fig2a <- ggplot(data=res2[res2$type != "neut",], aes(x=rank, y=logp, alpha=str, 
   geom_jitter(width = 0.04, size=3, alpha=0.8) + 
   ylab("-log10( p-value )") + 
   xlab("ranked evidence") +
-  ylim(c(0.7,5)) +
+  ylim(c(0,3)) +
   geom_point(x = orig$rank[orig$site == "chr5_96244549"],
-             y = -log10(1.2e-5),
+             y = orig$logp[orig$site == "chr5_96244549"],
              col="firebrick1", size=8) +
   # geom_point(x = res$rank[res$site == "chr2_204738938"],
   #            y = res$logp[res$site == "chr2_204738938"],
@@ -65,12 +65,11 @@ fig2a <- ggplot(data=res2[res2$type != "neut",], aes(x=rank, y=logp, alpha=str, 
   geom_point(x = orig$rank[orig$site == "chr18_77287776"],
              y = orig$logp[orig$site == "chr18_77287776"],
              col="firebrick1", size=8) +
-  geom_hline(yintercept = -log10(0.01), col='orange', lty=2) +
-  geom_hline(yintercept = -log10(0.001), col='red', lty=2) +
-  geom_hline(yintercept = -log10(0.001), col='red', lty=2) +
-  geom_hline(yintercept = -log10(0.05/1723), col='black', lty=2) +
+  geom_hline(yintercept = -log10(0.05), col='orange', lty=2) +
+  geom_hline(yintercept = -log10(0.01), col='red', lty=2) +
+  # geom_hline(yintercept = -log10(0.05/1723), col='black', lty=2) +
   theme_classic() + theme(legend.position="none") +
   scale_color_manual(values=cbPalette); fig2a
 
 
-saveRDS(fig2A, "./fig2a.RData")
+saveRDS(fig2a, "./fig2a_revision.RData")
