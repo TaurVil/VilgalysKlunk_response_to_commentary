d9982315c2cb7080f2367ad38c98963cbc057b52
TaurVil <41305538+TaurVil@users.noreply.github.com>
2025-01-17 17:37:01 -0600

    Compute statistics using a range of nearby neutral sites
    Identify Fst outliers relative to 50-all neutral sites, rather than only the 200 sites used in the main results

--- code/02_get_pvals.R
+++ code/02_get_pvals.R
@@ -1,9 +1,9 @@
-library(data.table); library(ggplot2); library(patchwork)
+library(data.table); library(ggplot2); library(patchwork); library(parallel)
 
 # n_nearby sets the number of neutral sites
 # num_lowfreq sets the number of populations allowed to not contain the alternate allele
 
-for (n_nearby in c(250)) { # 50, 100, 2000
+for (n_nearby in c(50, 100, 150, 200, 250, 300, 400, 2000)) { # 50, 100, 2000
   for (num_lowfreq in 1) {
   load("../DATA/fst_estimates.RData")
   
@@ -16,15 +16,17 @@ for (n_nearby in c(250)) { # 50, 100, 2000
   sqrt(summary(lm(info$maf.ML ~ info$maf.GL))$r.squared)
   
   ######## Split off neutral sites, then filter candidates based on minor allele frequency ########
-  info_neut <- info[info$type == "neut",]; info_neut$maf_rank <- rank(info_neut$maf.ML)
+  info_neut <- info[info$type == "neut",]
+  info_neut$maf_rank <- rank(info_neut$maf.ML)
   info <- info[info$maf.ML >= min_maf,]
   ########
   
   ######## calculate p-values ########
-  library(parallel); pvals <- info[,1:5]
+  pvals <- info[,1:5]
   
   calc_pval_L13 <- function(site) {
-    input_maf <- info$maf.ML[site]; input_fst <- info$london.post.pre.fst.ML[site]
+    input_maf <- info$maf.ML[site]
+    input_fst <- info$london.post.pre.fst.ML[site]
     n_less <- sum(info_neut$maf.ML <= input_maf)
     
     tmp_fst <- info_neut$london.post.pre.fst.ML[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2]
@@ -36,7 +38,8 @@ for (n_nearby in c(250)) { # 50, 100, 2000
   }
   pvals$L13.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pval_L13))
   calc_pval_L12 <- function(site) {
-    input_maf <- info$maf.ML[site]; input_fst <- info$london.during.pre.fst.ML[site]
+    input_maf <- info$maf.ML[site]
+    input_fst <- info$london.during.pre.fst.ML[site]
     n_less <- sum(info_neut$maf.ML <= input_maf)
     
     tmp_fst <- info_neut$london.during.pre.fst.ML[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2]
@@ -49,7 +52,8 @@ for (n_nearby in c(250)) { # 50, 100, 2000
   pvals$L12.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pval_L12))
   
   calc_pval_D13 <- function(site) {
-    input_maf <- info$maf.ML[site]; input_fst <- info$denmark.post.pre.fst.ML[site]
+    input_maf <- info$maf.ML[site]
+    input_fst <- info$denmark.post.pre.fst.ML[site]
     n_less <- sum(info_neut$maf.ML <= input_maf)
     
     tmp_fst <- info_neut$denmark.post.pre.fst.ML[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2]
@@ -72,8 +76,6 @@ for (n_nearby in c(250)) { # 50, 100, 2000
   candidates <- info$rsid[info$L13.pval.ML < 0.05 & info$D13.pval.ML < 0.1 & 
                        sign(info$delta_L13.ML) == sign(info$delta_D13.ML) &
                        sign(info$delta_L13.ML) != sign(info$delta_L12.ML)]
-  
-  # save.image(paste0("../DATA/for_michelle.pvalues.n_neutral_",n_nearby, ".max_low_freq_pops_", num_lowfreq,".neutralsites.RData"))
   
   save.image(paste0("../DATA/pvalues.n_neutral_",n_nearby, ".max_low_freq_pops_", num_lowfreq,".RData"))
   
--- code/04.section2_get_pvals.R
+++ code/04.section2_get_pvals.R
@@ -1,72 +1,87 @@
 
-library(data.table); library(ggplot2); library(patchwork)
+library(data.table); library(ggplot2); library(parallel); library(patchwork)
 
 load("./perms/permuted_ITERATION/fst.RData")
-n_nearby=250 # number of neutral loci to compare candidates against
 num_lowfreq=1 # maximum number of population-time points which we'll allow to not observe the alternate allele
-min_maf <- 0.05
-rsid_status <- 'keep_good_only'
-
 
 info <- as.data.frame(info)
 info <- info[rowSums(info[,which(colnames(info) %like% "alternate.ML")] < 0.01) <= num_lowfreq,]
 
+info -> info_all
 
-
-######## Split off neutral sites, then filter candidates based on minor allele frequency ########
-info_neut <- info[info$type == "neut",]; info_neut$maf_rank <- rank(info_neut$maf.ML)
-info <- info[info$maf.ML >= min_maf,]
-########
-
-######## calculate p-values ########
-library(parallel); pvals <- info[,1:5]
-
-calc_pval_L13 <- function(site) {
-  input_maf <- info$maf.ML[site]; input_fst <- info$london.post.pre.fst.ML[site]
-  n_less <- sum(info_neut$maf.ML <= input_maf)
+for (method in c("sliding")) {
+  if (method == "bins") (ns <- 200)
+  if (method == "sliding") (ns <- c(50, 100, 150, 200, 250, 300, 400, 2000))
   
-  tmp_fst <- info_neut$london.post.pre.fst.ML[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2]
-  if (length(tmp_fst) < n_nearby) {tmp_fst <- info_neut$london.post.pre.fst.ML[info_neut$maf_rank >= length(info_neut$maf_rank) - n_nearby]}
-  
-  tmp_percentiles <- ecdf(tmp_fst)
-  tmp_p <- 1-tmp_percentiles(input_fst)
-  return(tmp_p)
+  for (n_nearby in ns) {
+    for (min_maf in c(0.05)) {
+      for (rsid_status in "1kg") {
+        
+        ######## Split off neutral sites, then filter candidates based on minor allele frequency ########
+        info_neut <- info_all[info_all$type == "neut",]; info_neut$maf_rank <- rank(info_neut$maf.ML)
+        info <- info_all[info_all$maf.ML >= min_maf,]
+        
+        ## filter neutral sites as well if we're comparing all candidate vs all neutral
+        ## not introducing a floor where low MAF sites are only compared against higher maf sites
+        if (n_nearby == 2000) {info_neut <- info_neut[info_neut$maf.ML > min_maf,]}
+        
+        ########
+        
+        ######## keep sites in the rsid set ########
+        # already done in the first step
+        ########
+        
+        ######## Set bin sizes if using bins ##########
+        # Bins were removed in favor of sliding windows. For an example, see code for the original publication. 
+        ##########
+        
+        ######## calculate p-values ########
+        pvals <- info[,c(1:5,21,29,61:63)]
+        
+        get_tmp_fst_table <- function(input_maf) {
+          if (method == "sliding") {
+            n_less <- sum(info_neut$maf.ML <= input_maf)
+            tmp_fst <- info_neut[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2,]
+            if (nrow(tmp_fst) < n_nearby) {tmp_fst <- info_neut[info_neut$maf_rank >= length(info_neut$maf_rank) - n_nearby,]}
+          }
+          if (method == "bins") {
+            tmp_bin <- bins[input_maf > bins[,1] & input_maf <= bins[,2],]
+            tmp_fst <- info_neut[info_neut$maf.ML > tmp_bin[3] & info_neut$maf.ML <= tmp_bin[4]]
+          }
+          return(tmp_fst)
+        }
+        
+        calc_pvals <- function(site, test) {
+          input_maf <- info$maf.ML[site]
+          tmp_fst <- get_tmp_fst_table(input_maf)
+          
+          if (test == "L13") {
+            input_fst <- info$london.post.pre.fst.ML[site]
+            tmp_percentiles <- ecdf(tmp_fst$london.post.pre.fst.ML)
+          }
+          if (test == "L12") {
+            input_fst <- info$london.during.pre.fst.ML[site]
+            tmp_percentiles <- ecdf(tmp_fst$london.during.pre.fst.ML)
+          }
+          if (test == "D13") {
+            input_fst <- info$denmark.post.pre.fst.ML[site]
+            tmp_percentiles <- ecdf(tmp_fst$denmark.post.pre.fst.ML)
+          }
+          
+          tmp_p <- 1-tmp_percentiles(input_fst)
+          return(tmp_p)
+        }
+        
+        pvals$L13.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pvals, test="L13"))
+        pvals$L12.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pvals, test="L12"))
+        pvals$D13.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pvals, test="D13"))
+        ########
+
+        write.table(pvals, paste("./perms/permuted_ITERATION/pvalues.with_during", rsid_status, min_maf, method, n_nearby, "txt", sep="."), row.names = F, col.names=T, sep="\t", quote=F)
+        
+      }
+    }
+  }
 }
-pvals$L13.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pval_L13))
-calc_pval_L12 <- function(site) {
-  input_maf <- info$maf.ML[site]; input_fst <- info$london.during.pre.fst.ML[site]
-  n_less <- sum(info_neut$maf.ML <= input_maf)
-  
-  tmp_fst <- info_neut$london.during.pre.fst.ML[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2]
-  if (length(tmp_fst) < n_nearby) {tmp_fst <- info_neut$london.during.pre.fst.ML[info_neut$maf_rank >= length(info_neut$maf_rank) - n_nearby]}
-  
-  tmp_percentiles <- ecdf(tmp_fst)
-  tmp_p <- 1-tmp_percentiles(input_fst)
-  return(tmp_p)
-}
-pvals$L12.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pval_L12))
 
-calc_pval_D13 <- function(site) {
-  input_maf <- info$maf.ML[site]; input_fst <- info$denmark.post.pre.fst.ML[site]
-  n_less <- sum(info_neut$maf.ML <= input_maf)
-  
-  tmp_fst <- info_neut$denmark.post.pre.fst.ML[info_neut$maf_rank <= (n_less + n_nearby/2) & info_neut$maf_rank >= n_less - n_nearby/2]
-  if (length(tmp_fst) < n_nearby) {tmp_fst <- info_neut$denmark.post.pre.fst.ML[info_neut$maf_rank >= length(info_neut$maf_rank) - n_nearby]}
-  
-  tmp_percentiles <- ecdf(tmp_fst)
-  tmp_p <- 1-tmp_percentiles(input_fst)
-  return(tmp_p)
-}
-pvals$D13.pval <- do.call("c",mclapply(1:nrow(info), FUN = calc_pval_D13))
-
-rm(list=ls(pattern="calc_pval_"))
-rm(drop_samples_missing, design, design2, info_neut)
-########
-
-
-pvals <- pvals[,-c(1:4)]; colnames(pvals)[2:4] <- paste(colnames(pvals)[2:4],"ML", sep=".")
-info <- merge(pvals, info, by="site"); rm(pvals)
-########
-
-save.image("./perms/permuted_ITERATION/pvals.RData")
 
